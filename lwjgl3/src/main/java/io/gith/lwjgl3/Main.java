65848329ebe6e212f158fe09ff1a36dab87dd53e
Oleki <olekignaczak78@gmail.com>
2025-09-26 21:09:12 +0200

    Init new ver

---
+++ lwjgl3/src/main/java/io/gith/lwjgl3/Main.java
@@ -0,0 +1,31 @@
+package io.gith.lwjgl3;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Graphics;
+import imgui.ImGui;
+import imgui.ImGuiIO;
+import imgui.ImVec2;
+import imgui.flag.ImGuiCond;
+import imgui.flag.ImGuiWindowFlags;
+import imgui.gl3.ImGuiImplGl3;
+import imgui.glfw.ImGuiImplGlfw;
+
+public class Main extends Game
+{
+    public void create()
+    {
+        System.out.println("a");
+    }
+
+    public void render()
+    {
+
+    }
+
+    public static void initImGui() {
+
+    }
+}
